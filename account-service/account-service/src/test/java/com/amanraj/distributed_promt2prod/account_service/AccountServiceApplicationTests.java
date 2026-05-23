0ef4d7f1f8e7fca88003c2899921a62f53de7e06
Aman Raj <amanraj700482@gmail.com>
2026-05-24 01:24:57 +0530

    decompose and setup monolith system into microservices architecture

---
+++ account-service/account-service/src/test/java/com/amanraj/distributed_promt2prod/account_service/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amanraj.distributed_promt2prod.account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ workspace-service/workspace-service/src/test/java/com/amanraj/distributed_promt2prod/workspace_service/WorkspaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amanraj.distributed_promt2prod.workspace_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
